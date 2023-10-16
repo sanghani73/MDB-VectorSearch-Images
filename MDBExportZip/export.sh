89c85534994a6941b0b11855ccc6164e41296a1a
Anand Sanghani <anand@sanghani.net>
2023-10-16 08:51:39 +0100

    added compressed mongodump files

---
+++ MDBExportZip/export.sh
@@ -0,0 +1,2 @@
+# run the following to take an export of the vector_search.products collection
+mongodump --uri mongodb+srv://admin:admin123@vftechday.hluaz.mongodb.net --db=vector_search --collection=products --gzip 
---
+++ MDBExportZip/import.sh
@@ -0,0 +1,2 @@
+# run the following to take an export of the vector_search.products collection
+mongorestore --uri mongodb+srv://admin:admin123@vftechday.hluaz.mongodb.net --gzip dump/
